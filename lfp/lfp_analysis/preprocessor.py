b203dfdf1193a8578c709970edc9389a161091b0
mcum96 <meghanic96@gmail.com>
2024-11-16 14:59:07 -0500

    finish code and test for map_to_region

--- lfp/lfp_analysis/preprocessor.py
+++ lfp/lfp_analysis/preprocessor.py
@@ -1,12 +1,18 @@
 import numpy as np
+from bidict import bidict
 
 
-def map_to_region(traces, subject_region_dict):
-    traces_dict = {}
-    for region, channel in subject_region_dict.items():
-        traces_dict[region] = traces[int(channel), :]
+def map_to_region(all_traces, subject_region_dict):
+    # sort brain regions by channel in incresing order
+    sorted_regions = [k for k, v in sorted(subject_region_dict.items(), key=lambda x: x[1])]
+    # sort associated selected channels in incresing order
+    sorted_channels = [v for k, v in sorted(subject_region_dict.items(), key=lambda x: x[1])]
+    # select only desired traces
+    traces = all_traces[sorted_channels, ...]
+    # create a bidict for brain region to index of new trace array
+    brain_region_dict = bidict({region: idx for idx, region in enumerate(sorted_regions)})
 
-    return traces_dict
+    return brain_region_dict, traces
 
 
 def zscore():
--- lfp/tests/test_single_small_file.py
+++ lfp/tests/test_single_small_file.py
@@ -16,13 +16,13 @@ class test_lfp_recording_preprocessing(unittest.TestCase):
 
         brain_regions, traces = preprocessor.map_to_region(all_traces_arr, SUBJECT_DICT)
         # brain regions is a bidict: {brain_region: brainregion_index}
-        self.assertIs(type(brain_regions), bidict.Bidict)
-        self.assertCountEqual(
-            brain_regions.keys(), ["mPFC", "vHPC", "BLA", "NAc", "MD"]
-        )
+        self.assertIs(type(brain_regions), bidict.bidict)
+        self.assertCountEqual(brain_regions.keys(), ["mPFC", "vHPC", "BLA", "NAc", "MD"])
         # traces is numpy array [ brainregion_index, timebins ]
         self.assertIs(type(traces), np.ndarray)
 
         # All of the indexes in brain_regions exist in traces
         for each in brain_regions.items():
-            self.assertEqual(traces[each[1]].shape, (1000,))
+            self.assertEqual(traces[each[1]].shape, (2500,))
+        self.assertEqual(brain_regions["mPFC"], 0)
+        self.assertEqual(brain_regions["vHPC"], 4)
