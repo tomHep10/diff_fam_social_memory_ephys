45cba75957df12c27a7813b4b7e812ce3274dd13
mcum96 <meghanic96@gmail.com>
2024-11-17 12:41:40 -0500

    updated names and added multitaper connectiviity functions

---
+++ lfp/lfp_analysis/connectivity_wrapper.py
@@ -0,0 +1,47 @@
+from spectral_connectivity import Multitaper, Connectivity
+
+
+def calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep):
+    multi_t = Multitaper(
+        # multitaper takes in a time_series that is time by signals (regions)
+        time_series=rms_traces.T,
+        sampling_frequency=downsample_rate,
+        time_halfbandwidth_product=halfbandwidth,
+        time_window_duration=timewindow,
+        time_window_step=timestep,
+    )
+    connectivity = Connectivity.from_multitaper(multi_t)
+    frequencies = connectivity.frequencies
+    return connectivity, frequencies
+
+
+def calculate_power(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep):
+    connectivity = calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
+    # connectivity.power.() = [timebins, frequencies, signal]
+    power = connectivity.power()
+    return power
+
+
+def calculate_phase():
+    return
+
+
+def calculate_coherence(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep):
+    connectivity = calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
+    # calculates a matrix of timebins, frequencies, region, region
+    # such that [x,y,a,a] = nan
+    # and [x,y,a,b] = [x,y,b,a] which is the coherence between region a & b
+    # for frequency y at time x
+    coherence = connectivity.coherence_magnitude()
+    return coherence
+
+
+def calculate_grangers(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep):
+    connectivity = calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
+    # calculates a matrix of timebins, frequencies, region, region
+    # such that [x,y,a,a] = nan
+    # and [x,y,a,b] =/= [x,y,b,a]
+    # [x,y,a,b] -> a to b granger?
+    # [x,y,b,a] -> b to a granger?
+    granger = connectivity.pairwise_spectral_granger_prediction()
+    return granger
--- lfp/tests/test_connectivity_wrapper.py
+++ lfp/tests/test_connectivity_wrapper.py
@@ -13,6 +13,11 @@ class test_lfp_recording_power(unittest.TestCase):
         traces_path = os.path.join("tests", "test_data", "test_traces.csv")
         all_traces_arr = np.loadtxt(traces_path, delimiter=",")
         rms_traces, brain_regions = preprocessor.preprocess(all_traces_arr, SUBJECT_DICT, 0.2, 0.195)
-
-        power_signal, frequencies = power.calculate_power(rms_traces, 500, 1, 1, 0.01)
-        self.assertEqual(power_signal.shape, (5, 2500))
+        print(rms_traces.shape)
+        power_signal, frequencies = power.calculate_power(rms_traces, 200, 2, 1, 0.5)
+        print(frequencies)
+        # power = [samples, frequenceis, signals]
+        # signals = traces.shape[0] or how many brain regions
+        # frequencies = sampling_frequency * time_window_duration * time_window_step
+        # samples =
+        self.assertEqual(power_signal.shape, (25, 50, 5))
