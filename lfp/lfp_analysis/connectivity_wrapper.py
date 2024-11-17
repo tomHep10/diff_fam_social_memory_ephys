e595c2b3de37c5caf7f64992509ebec52e2c64b9
Abraham Q <abraham@cavela.com>
2024-11-17 13:23:48 -0500

    Adds test for connectivityWrapper

--- lfp/lfp_analysis/connectivity_wrapper.py
+++ lfp/lfp_analysis/connectivity_wrapper.py
@@ -1,6 +1,14 @@
 from spectral_connectivity import Multitaper, Connectivity
 
 
+def connectivity_wrapper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep):
+    connectivity, frequencies = calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
+    power = calculate_power(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
+    coherence = calculate_coherence(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
+    granger = calculate_grangers(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
+    return connectivity, frequencies, power, coherence, granger
+
+
 def calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep):
     multi_t = Multitaper(
         # multitaper takes in a time_series that is time by signals (regions)
@@ -16,7 +24,7 @@ def calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow,
 
 
 def calculate_power(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep):
-    connectivity = calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
+    connectivity, frequencies = calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
     # connectivity.power.() = [timebins, frequencies, signal]
     power = connectivity.power()
     return power
@@ -27,7 +35,7 @@ def calculate_phase():
 
 
 def calculate_coherence(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep):
-    connectivity = calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
+    connectivity, frequencies = calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
     # calculates a matrix of timebins, frequencies, region, region
     # such that [x,y,a,a] = nan
     # and [x,y,a,b] = [x,y,b,a] which is the coherence between region a & b
@@ -37,7 +45,7 @@ def calculate_coherence(rms_traces, downsample_rate, halfbandwidth, timewindow, 
 
 
 def calculate_grangers(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep):
-    connectivity = calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
+    connectivity, frequencies = calculate_multitaper(rms_traces, downsample_rate, halfbandwidth, timewindow, timestep)
     # calculates a matrix of timebins, frequencies, region, region
     # such that [x,y,a,a] = nan
     # and [x,y,a,b] =/= [x,y,b,a]
--- lfp/tests/test_connectivity_wrapper.py
+++ lfp/tests/test_connectivity_wrapper.py
@@ -2,22 +2,21 @@ import numpy as np
 import unittest
 import os
 import lfp_analysis.preprocessor as preprocessor
-import lfp_analysis.power as power
+import lfp_analysis.connectivity_wrapper as connectivity_wrapper
 
 SUBJECT_DICT = {"mPFC": 19, "vHPC": 31, "BLA": 30, "NAc": 28, "MD": 29}
 SPIKE_GADGETS_MULTIPLIER = 0.6745
 
 
 class test_lfp_recording_power(unittest.TestCase):
-    def test_calculate_power(self):
+    def test_calculate_all_connectivity_small_file(self):
         traces_path = os.path.join("tests", "test_data", "test_traces.csv")
         all_traces_arr = np.loadtxt(traces_path, delimiter=",")
         rms_traces, brain_regions = preprocessor.preprocess(all_traces_arr, SUBJECT_DICT, 0.2, 0.195)
-        print(rms_traces.shape)
-        power_signal, frequencies = power.calculate_power(rms_traces, 200, 2, 1, 0.5)
-        print(frequencies)
-        # power = [samples, frequenceis, signals]
-        # signals = traces.shape[0] or how many brain regions
-        # frequencies = sampling_frequency * time_window_duration * time_window_step
-        # samples =
-        self.assertEqual(power_signal.shape, (25, 50, 5))
+
+        connectivity, frequencies, power, coherence, granger = connectivity_wrapper.connectivity_wrapper(
+            rms_traces, 200, 2, 1, 0.5
+        )
+
+        self.assertEqual(power.shape, (24, 100, 5))
+        self.assertEqual(coherence.shape, (24, 100, 5, 5))
